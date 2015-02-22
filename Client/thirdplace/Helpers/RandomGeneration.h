e7bf73a0f4cb49418f149e8be90fad3d21ad139f
Stephen Karpinskyj <stephen.karpinskyj@gmail.com>
2015-02-22 17:26:26 +1300

    Add initial XMPP extensions

---
+++ Client/thirdplace/Helpers/RandomGeneration.h
@@ -0,0 +1,13 @@
+//
+//  RandomGeneration.h
+//  thirdplace
+//
+//  Created by Stephen Karpinskyj on 22/02/15.
+//  Copyright (c) 2015 Hunch Pty Ltd. All rights reserved.
+//
+
+#ifndef thirdplace_RandomGeneration_h
+#define thirdplace_RandomGeneration_h
+
+
+#endif
---
+++ Client/xmpp/XMPPFramework.m
@@ -0,0 +1,9 @@
+//
+//  XMPPFramework.m
+//  thirdplace
+//
+//  Created by Stephen Karpinskyj on 22/02/15.
+//  Copyright (c) 2015 Hunch Pty Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
